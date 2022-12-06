382b6a07d9608ebfefd2bab384e2e85ae7c28b40
cellaaudi <cellaaudi27@gmail.com>
2022-12-07 00:02:09 +0700

    crash

--- index.php
+++ index.php
@@ -1,12 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Memes</title>
+    <link rel="stylesheet" href="css/index.css">
 </head>
+
 <body>
-    
+    <div id="container">
+        <section>
+            <div class="card">
+                <div class="img">
+                    <img src="https://i.pinimg.com/564x/aa/81/13/aa81136d4c3fe75fd31ae61e639bcedf.jpg" alt="">
+                </div>
+                <div class="card_action">
+                    <div class="likes">
+                        <img src="assets/icons/heart_outline.svg" alt="">
+                        <span>200 likes</span>
+                    </div>
+                    <div class="comments">
+                        <img src="assets/icons/comment_fill.svg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://i.pinimg.com/564x/ac/78/4a/ac784adfd60244a60e48ec3732ab147f.jpg" alt="">
+                </div>
+                <div class="card_action">
+                    <div class="likes">
+                        <img src="assets/icons/heart_outline.svg" alt="">
+                        <span>200 likes</span>
+                    </div>
+                    <div class="comments">
+                        <img src="assets/icons/comment_fill.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
+
 <body>
-    
+    <div id="container">
+        <div id="wrapper">
+            <div>LOG IN</div>
+            <div class="login"><input type="text" placeholder="username"></div>
+            <div class="login"><input type="password" placeholder="password"></div>
+            <div class="login"><input type="submit" value="Login"></div>
+        </div>
+    </div>
 </body>
+
 </html>
