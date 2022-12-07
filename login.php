8fa60cb3f8be6d9def40271325308c8720f84c59
ketketin <114962909+ketketin@users.noreply.github.com>
2022-12-07 09:52:35 +0700

    update klo masuk index.php lgsg ke login

--- login.php
+++ login.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+if (isset($_GET['do'])) {
+    unset($_SESSION['user_login']);
+    session_destroy();
+} elseif (isset($_SESSION['user_login'])) {
+    header("location: index.php");
+}
 // if (isset($_POST['login'])) {}
 // require("class/user.php");
 
@@ -40,7 +47,7 @@
     <div id="container">
         <div id="wrapper">
             <div>LOG IN</div>
-            <form action="" method="post" id="formLogin">
+            <form action="login_process.php" method="post" id="formLogin">
                 <div class="login"><input type="text" placeholder="username" name="username"></div>
                 <div class="login"><input type="password" placeholder="password" name="password"></div>
                 <div class="login"><input type="submit" value="Login" name="login"></div>
